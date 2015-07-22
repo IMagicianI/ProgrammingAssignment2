bfa1b6d880a97fa69ee67fc2d285881f529fa538
IMagicianI <alariv8@gmail.com>
2015-07-22 19:50:40 +0300

    R 2. assignment cachematrix complete

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# makeCacheMatrix: This function creates a special "matrix" object that can cache its matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        ##                                               input: x has to be an invertible matrix
+        ##                                                output: list of 4 functions which set and get the matrix and its inverse
+        ##                                                the list is used as the input to cacheSolve()
+        i <- NULL 
+        set <- function(y){                             # set MATRIX outside global environment (specialized <<- operator)
+                x <<- y
+                i <<- NULL                              # set invmat = NULL outside global environment
+        }
+        get <- function()x                              # free variables looked for in the function in which they are defined, here in set function environment
+        setinvmat <- function(invmat) i <<- invmat      # function setinvmat takes the inverse matrix and assigns it to i
+        getinvmat <- function() i                        #function getinvmat returns the value of the inverse matrix of x
+        list(set = set, get = get,                       # makevector returns this list of 4 functions which set and get the matrix and its inverse
+             setinvmat = setinvmat,
+             getinvmat = getinvmat)
 }
 
-
-## Write a short comment describing this function
+# follows: functionlist_as_variable$specified_function 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## x is the output of makeCacheMatrix()
+        ## cacheSolve returns the inverse of the matrix inputted to makeCacheMatrix
+        i <- x$getinvmat()                                      # use getter to get the inverse matrix
+       
+        if(!(is.null(i))){                                      # if the inverse matrix has been set
+                message("getting cached data")                  # then display "getting cached data"
+                return(i)                                        # and return the inverse
+        }
+        # otherwise calculate the invmat
+        data <- x$get()                                         # gets the matrix using getter
+        invmat <- solve(data, ...)                              # stores its inverse matrix to invmat
+        x$setinvmat(invmat)                                     # sets invmat as the inverse matrix of the initial input matrix
+        
+        return(invmat)                                           # returns the inverse matrix of 'x'
+         
 }
